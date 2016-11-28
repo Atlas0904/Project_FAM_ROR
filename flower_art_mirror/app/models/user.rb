c4e5bfc6118ee12a95fa80d4875a323c34d90f78
atlas_huang <atlas_huang@htc.com>
2016-11-28 17:13:07 +0800

    Include awesome print & Opt UI

--- flower_art_mirror/app/models/user.rb
+++ flower_art_mirror/app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   
   has_secure_password validations: false
 
-  has_attached_file :photo
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
   validates :name, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 5}
